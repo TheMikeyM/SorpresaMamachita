fa5aec934fa97530fc330068cf7b6c86bae5ba88
TheMikeyM <michaeldogor12@gmail.com>
2026-04-20 22:38:03 -0500

    Update fotos.js

--- fotos.js
+++ fotos.js
@@ -5,11 +5,11 @@
    ========================================================== */
 
 const FOTOS = [
-  { nombre: "Fotito 1", src: "fotitos/fotito1.jpg" },
-  { nombre: "Fotito 2", src: "fotitos/fotito2.jpg" },
-  { nombre: "Fotito 3", src: "fotitos/fotito3.jpg" },
-  { nombre: "Fotito 4", src: "fotitos/fotito4.jpg" },
-  { nombre: "Fotito 5", src: "fotitos/fotito5.jpg" },
+  { nombre: "Fotito 1", src: "fotitos/fotito1.jpeg" },
+  { nombre: "Fotito 2", src: "fotitos/fotito2.jpeg" },
+  { nombre: "Fotito 3", src: "fotitos/fotito3.jpeg" },
+  { nombre: "Fotito 4", src: "fotitos/fotito4.jpeg" },
+  { nombre: "Fotito 5", src: "fotitos/fotito5.jpeg" },
   { nombre: "Fotito 6", src: "fotitos/fotito6.jpg" },
   { nombre: "Fotito 7", src: "fotitos/fotito7.jpg" },
   { nombre: "Fotito 8", src: "fotitos/fotito8.jpg" },
